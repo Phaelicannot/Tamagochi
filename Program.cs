4d2ed6ed7717d936f3ee86025bea3e3b3923b672
Phaelicannot <phaelica.w@gmail.com>
2023-10-06 12:47:02 +0200

    försöker fixa basic funktioner

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿
+using Microsoft.VisualBasic;
+
+Console.WriteLine("Welcome! I will now throw you the responsibility of taking care of this creature.");
+
+tamagochi eggcreature = new tamagochi();
+
+Console.WriteLine("What do you want to name the little guy?");
+eggcreature.name = Console.ReadLine();
+
+Console.WriteLine($"{eggcreature.name}? Sounds like a nice name!");
+Console.ReadLine();
+Console.WriteLine("I think...");
+Console.ReadLine();
+Console.WriteLine("Anyways! You will have to feed and play with your little friend, or else it will die.");
+Console.ReadLine();
+Console.WriteLine("But you wouldn't let such an innocent creature die, would you? I trust you. Treat it well. No pressure.");
+Console.ReadLine();
+Console.WriteLine("*Write the numbers corresponding to the action you want to do.*");
+Console.ReadLine();
+
+eggcreature.act = Console.ReadLine();
+while (eggcreature.act != "1" && eggcreature.act != "2" && eggcreature.act != "3")
+{
+    Console.WriteLine("1: Feed          2: Hi         3: Teach          4: Play");
+    if (eggcreature.act != "1" && eggcreature.act != "2" && eggcreature.act != "3")
+    {
+        System.Console.WriteLine("Sorry?");
+    }
+}
+
+if (eggcreature.act == "1")
+{
+    int food = Console.ReadLine();
+    if (food == 1)
+    {
+        
+    }
+    eggcreature.Feed();
+}
+if (eggcreature.act == "2")
+{
+
+}
+if (eggcreature.act == "3")
+{
+    Console.WriteLine($"What word would you like to teach {eggcreature.name}?");
+    string newWord = Console.ReadLine();
+    eggcreature.Teach();
+}
+
+
+
+
---
+++ tamagochi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class tamagochi
+{
+    Random generator = new Random();
+    public string name;
+    public string act;
+    private int hungerLevel;
+    private int boredomLevel;
+    private bool isAlive;
+    List<string> words = new();
+    List<int> foods = new()
+    {
+        1, 3, 2
+    };
+    // Dictionary<string, int> foods = new(){
+    //     {"carrot", 1},
+    //     {"apple pie", 3},
+    //     {"tears of your enemies", 2}
+    // };
+
+
+
+    public bool GetAlive();
+    public void Tick();
+    public void Hi();
+    public void Feed()
+    {
+        hungerLevel -= 2;
+    }
+    public void Teach()
+    {
+        words.Add(newWord);
+    }
+    public void PrintStats();
+    private void ReduceBoredom();
+
+
+
+
+}
