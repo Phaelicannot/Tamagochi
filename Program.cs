6a32043bc14011c1a7fc8ded1642a85074864d0b
Phaelicannot <phaelica.w@gmail.com>
2023-10-13 12:26:47 +0200

    gjorde klart den

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualBasic;
 
 Console.WriteLine("Welcome! I will now throw you the responsibility of taking care of this creature.");
 
-tamagochi eggcreature = new tamagochi();
+tamagotchi eggcreature = new tamagotchi();
 
 Console.WriteLine("What do you want to name the little guy?");
 eggcreature.name = Console.ReadLine();
@@ -17,38 +17,58 @@ Console.ReadLine();
 Console.WriteLine("But you wouldn't let such an innocent creature die, would you? I trust you. Treat it well. No pressure.");
 Console.ReadLine();
 Console.WriteLine("*Write the numbers corresponding to the action you want to do.*");
+
+eggcreature.GetAlive();
+
+while (eggcreature.GetAlive() == true)
+{
+    while (eggcreature.act != "1" && eggcreature.act != "2" && eggcreature.act != "3" && eggcreature.act != "4" && eggcreature.act != "5")
+    {
+        Console.WriteLine("1: Feed          2: Hi         3: Teach          4: Play          5: Wait");
+        eggcreature.act = Console.ReadLine();
+        if (eggcreature.act != "1" && eggcreature.act != "2" && eggcreature.act != "3" && eggcreature.act != "4" && eggcreature.act != "5")
+        {
+            System.Console.WriteLine("Sorry?");
+        }
+    }
+
+    if (eggcreature.act == "1")
+    {
+        Console.WriteLine($"What do you want to feed {eggcreature.name}? Do not overfeed it, or it will die.");
+        Console.WriteLine("1: Carrot (-2 hunger)           2: Peas (-1 hunger)           3: Apple (-3 hunger)            4: Nevermind");
+        eggcreature.Feed();
+    }
+    if (eggcreature.act == "2")
+    {
+        Console.WriteLine($"You say hi to {eggcreature.name}");
+        eggcreature.Hi();
+    }
+    if (eggcreature.act == "3")
+    {   
+        Console.WriteLine($"What word would you like to teach {eggcreature.name}? Type '1' if you want to go back");
+        string newWord = Console.ReadLine();
+        eggcreature.Teach(newWord);
+    }
+    if (eggcreature.act == "4")
+    {
+        Console.WriteLine($"You play with {eggcreature.name}");
+        eggcreature.Play();
+    }
+    if (eggcreature.act == "5")
+    {
+        Console.WriteLine($"You let {eggcreature.name} sit and stare into a wall. Pretty boring.");
+        eggcreature.Wait();
+    }
+    eggcreature.Tick();
+    if (eggcreature.GetAlive() == true)
+    {
+        eggcreature.PrintStats();
+    }
+    eggcreature.act = "0";
+}
+
+Console.WriteLine($"{eggcreature.name} died");
+Console.WriteLine("Wow, I should have known... To be honest, I think a potato could do a better job than you.");
 Console.ReadLine();
-
-eggcreature.act = Console.ReadLine();
-while (eggcreature.act != "1" && eggcreature.act != "2" && eggcreature.act != "3")
-{
-    Console.WriteLine("1: Feed          2: Hi         3: Teach          4: Play");
-    if (eggcreature.act != "1" && eggcreature.act != "2" && eggcreature.act != "3")
-    {
-        System.Console.WriteLine("Sorry?");
-    }
-}
-
-if (eggcreature.act == "1")
-{
-    int food = Console.ReadLine();
-    if (food == 1)
-    {
-        
-    }
-    eggcreature.Feed();
-}
-if (eggcreature.act == "2")
-{
-
-}
-if (eggcreature.act == "3")
-{
-    Console.WriteLine($"What word would you like to teach {eggcreature.name}?");
-    string newWord = Console.ReadLine();
-    eggcreature.Teach();
-}
-
-
 
 
--- tamagochi.cs
+++ tamagochi.cs
@@ -1,39 +1,163 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public class tamagochi
+public class tamagotchi
 {
     Random generator = new Random();
     public string name;
     public string act;
+    private int boredom;
+    private int tickCount = 0;
+    private bool nothing = false;
     private int hungerLevel;
     private int boredomLevel;
-    private bool isAlive;
-    List<string> words = new();
-    List<int> foods = new()
+    private bool isAlive = true;
+    private string food;
+    List<string> words = new()
     {
-        1, 3, 2
+        "squeak"
     };
-    // Dictionary<string, int> foods = new(){
-    //     {"carrot", 1},
-    //     {"apple pie", 3},
-    //     {"tears of your enemies", 2}
-    // };
+   
+    
 
-
-
-    public bool GetAlive();
-    public void Tick();
-    public void Hi();
+    public bool GetAlive()
+    {
+        if (isAlive == false)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    public void Tick()
+    {
+        if (nothing == false)
+        {
+            hungerLevel++;
+            boredomLevel++;
+            tickCount++;
+            if (tickCount == 1)
+            {
+                Console.WriteLine($"By the way, just because I can, I have made {name} get extra hungry and bored every 5 rounds from now on.");
+            }
+            if (tickCount >= 7)
+            {
+                boredomLevel++;
+                hungerLevel++;
+                Console.WriteLine($"{name} gets extra hungry and bored this round.");
+                tickCount = 2;
+            }
+            if (hungerLevel > 10 || boredomLevel > 10)
+            {
+                isAlive = false;
+            }
+            else
+            {
+                isAlive = true;
+            }
+        }
+        else 
+        {
+            nothing = false;
+        }
+    }
+    public void Wait()
+    {
+        int bored = generator.Next(3);
+        boredomLevel += bored;
+    }
+    public void Hi()
+    {
+        if (boredomLevel >= 2)
+        {
+            int whatWord = generator.Next(words.Count);
+            Console.WriteLine($"{name} responds with '{words[whatWord]}'");
+            ReduceBoredom(3);
+        }
+        else
+        {
+            Console.WriteLine($"{name} doesn't want to speak with you right now. Try again when it is more bored.");
+        }
+    }
+    public void Play()
+    {
+        if (boredomLevel >= 4)
+        {
+            Console.WriteLine($"You play with {name}.");
+            Console.WriteLine($"{name} becomes less bored, but a little more hungry.");
+            ReduceBoredom(4);
+            hungerLevel++;
+        }
+        else
+        {
+            Console.WriteLine($"{name} doesn't want to play with you right now. Try again when it is more bored.");
+        }
+    }
     public void Feed()
     {
-        hungerLevel -= 2;
+        food = Console.ReadLine();
+        if (food == "1")
+        {
+            hungerLevel -= 3;
+            Console.WriteLine($"You feed {name} a carrot");
+        }
+        if (food == "2")
+        {
+            hungerLevel -= 2;
+            Console.WriteLine($"You feed {name} some peas");
+        }
+        if (food == "3")
+        {
+            hungerLevel -= 4;
+            Console.WriteLine($"You feed {name} an apple");
+        }
+        if (food == "4")
+        {
+            nothing = true;
+            act = "0";
+        }
+        if (hungerLevel <= -2)
+        {
+            hungerLevel = 10;
+            Console.WriteLine($"You overfed {name}.");
+        }
+        if (food != "4")
+        {
+            Console.WriteLine($"{name} becomes less hungry.");
+        }
     }
-    public void Teach()
+    public void Teach(string newWord)
     {
-        words.Add(newWord);
+        if (newWord == "1")
+        {
+            nothing = true;
+            act = "0";
+        }
+        else
+        {
+            Console.WriteLine($"You taught {name} the word: {newWord}");
+            words.Add(newWord);
+            Console.WriteLine($"{name} found it slightly entertaining.");
+            ReduceBoredom(3);
+        }
+       
     }
-    public void PrintStats();
-    private void ReduceBoredom();
+    public void PrintStats()
+    {
+        Console.WriteLine($"Hunger: {hungerLevel}");
+        Console.WriteLine($"Boredom: {boredomLevel}");
+    }
+
+    private void ReduceBoredom(int boredom)
+    {
+        boredomLevel -= boredom;
+        if (boredomLevel < 0)
+        {
+            boredomLevel = 0;
+        }
+    }
 
 
 
